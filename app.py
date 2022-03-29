71de8bba6b7a205ec27bc06d47870874a2225bec
Benedictus Kent <benedictuskent123@gmail.com>
2022-03-29 10:24:33 +0800

    final update code, training and submission data

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     # Best model: SARIMAX(2, 1, 3)x(1, 0, [1], 7)
     model = SARIMAX(df_training['備轉容量(MW)'], order=(2,1,3), seasonal_order=(1,0,1,7))
     model = model.fit()
-    index_future_dates = pd.date_range(start='2022-03-23', end='2022-04-06')
-    df_result = model.predict(start=len(df_training), end=len(df_training)+14, typ='levels')
+    index_future_dates = pd.date_range(start='2022-03-30', end='2022-04-13')
+    df_result = model.predict(start=len(df_training)+3, end=len(df_training)+17, typ='levels')
     df_result.index = index_future_dates
     df_result.to_csv(args.output, header=False)
